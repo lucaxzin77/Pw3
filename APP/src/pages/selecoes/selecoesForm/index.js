527b351d788f1fcca13f90d1f928d903132e693a
Vitor Migliani <vitormiglianivpn@gmail.com>
2026-03-11 22:32:36 +0000

    Cadastro e exclusao de Selecoes finalizado!

---
+++ APP/src/pages/selecoes/selecoesForm/index.js
@@ -0,0 +1,40 @@
+import { postSelecao } from "../../../services/selecoes.service.js";
+
+const form = document.getElementById("formPostSelecao");
+
+const inputNome = document.getElementById("nomeSelecao");
+const inputTecnico = document.getElementById("tecnicoSelecao");
+const inputLogo = document.getElementById("logoSelecao");
+const selectGrupo = document.getElementById("grupoSelect");
+
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+
+const flagPreview = document.getElementById("flagPreview");
+
+inputLogo.addEventListener("change", ()=>{
+    flagPreview.src = inputLogo.value;
+})
+
+const createSelecao = async(data) =>{
+    try{
+        const result = await postSelecao(data);
+        return result;
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    const data = {
+        nome: inputNome.value,
+        tecnico: inputTecnico.value,
+        logo: inputLogo.value,
+        grupo: selectGrupo.value,
+        cores: [color1.value, color2.value]
+    }
+
+    const result = await createSelecao(data);
+    console.log("Seleção criada:", result);
+})
--- APP/src/pages/selecoes/selecoesLista/index.js
+++ APP/src/pages/selecoes/selecoesLista/index.js
@@ -1,11 +1,20 @@
-import { getSelecoes } from '../../../services/selecoes.service.js';
+import { getSelecoes, deleteSelecao } from '../../../services/selecoes.service.js';
 
 let selecoesContainer = document.querySelector(".selecoesContainer");
+
+const delSelecao = async (id)=>{
+    try{
+        const result = await deleteSelecao(id);
+        return result;
+    }catch(error){
+        console.log(error.message);
+    }
+}
 
 const renderSelecoes = (lista) => {
     lista.forEach(selecao => {
         selecoesContainer.innerHTML += `
-        <div class="w-[200px] h-[250px] p-[20px] bg-white rounded-lg shadow-lg shadow-${selecao.cores[0]}-700/50
+        <div class="w-[200px] h-[250px] p-[20px] bg-white rounded-lg shadow-lg shadow-[${selecao.cores[0]}]/50
         flex flex-col items-center justify-center gap-[6px] bg-white">
         <div class="max-w-[100px]">
             <img class="w-[100px] h-[100px] object-contain" src="${selecao.logo}" alt="">
@@ -18,15 +27,36 @@ const renderSelecoes = (lista) => {
             <span class="font-bold">Grupo: </span>
             <span>${selecao.grupo}</span>
         </div>
-        <div class="m-[5px]">
-            <button class="p-[5px] w-[60px] shadow-lg rounded-lg cursor-pointer bg-white transition-all duration-[0.3s] hover:bg-slate-200 active:scale-90">
+        <div class="m-[5px] text-center">
+            <button class="p-[5px] w-[80px] shadow-lg rounded-lg cursor-pointer bg-white transition-all duration-[0.3s] hover:bg-slate-200 active:scale-90">
                 Ver Mais
+            </button>
+            <button data-id="${selecao.id}" id="btnDelete" class="cursor-pointer text-zinc-700 m-[5px] p-[5px] transition-all duration-[0.3s] shadow-sm shadow-red-700/50 rounded-lg hover:scale-110 hover:bg-red-500 hover:text-white active:scale-90 active:bg-red-800 active:text-zinc-700">
+                <i class="fa-solid fa-trash-can"></i>
             </button>
         </div>
     </div>
         `
     });
 }
+
+selecoesContainer.addEventListener("click", async (e)=>{
+    e.preventDefault();
+
+    const button = e.target.closest("#btnDelete");
+
+    if (!button) return;
+
+    const id = button.dataset.id;
+
+    const confirmar = confirm("Tem certeza que deseja excluir esta seleção?");
+
+    if (!confirmar) return;
+
+    await delSelecao(id);
+
+    location.reload();
+})
 
 const selecoes = await getSelecoes();
 renderSelecoes(selecoes);
